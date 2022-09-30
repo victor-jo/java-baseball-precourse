a28e8d2ad27cedafd926a1c448d55b16a4b12fb1
victor-jo <joasjn45@gmail.com>
2022-10-01 08:47:11 +0900

    style(main) : 불필요한 임포트 구문 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,8 @@ package baseball;
 import baseball.factory.ComputerFactory;
 import baseball.factory.GameRestartPolicyFactory;
 import baseball.factory.PlayerFactory;
-import baseball.game.Computer;
 import baseball.game.Game;
-import baseball.game.GameRestartPolicy;
 import baseball.game.GameStarter;
-import baseball.game.Player;
 
 public class Application {
     public static void main(String[] args) {
