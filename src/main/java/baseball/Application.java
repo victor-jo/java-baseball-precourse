73ccc22cbc1aa9fcd6b853025b20f345fa5d2803
victor-jo <joasjn45@gmail.com>
2022-09-30 23:38:57 +0900

    feat(main) : 게임 실행로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import baseball.computer.Computer;
+import baseball.game.Game;
+import baseball.game.GameRestartPolicy;
+import baseball.game.GameStarter;
+import baseball.player.Player;
+import baseball.player.PlayerNumbersValidator;
+import baseball.printer.ConsolePrinter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(
+            new Computer(
+                new ConsolePrinter()
+            ),
+            new Player(
+                new ConsolePrinter(),
+                new PlayerNumbersValidator()
+            ),
+            new GameRestartPolicy(
+                new ConsolePrinter()
+            )
+        );
+        new GameStarter(game).start();
     }
 }
